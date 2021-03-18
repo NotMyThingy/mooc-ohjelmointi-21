787d101813a310629b6a24ca1eeb72e5b07681ae
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-18 16:04:12 +0200

    spellchecker

--- osa06-07_spellchecker/src/spellchecker.py
+++ osa06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,22 @@
 # tee ratkaisu tänne
+def sanakirja():
+    sanat = []
+    with open('wordlist.txt') as tiedosto:
+        for sana in tiedosto:
+            sanat.append(sana.strip())
+    return sanat
+
+
+def main():
+    sanat = sanakirja()
+    teksti = input("Write text: ")
+    for sana in teksti.split(' '):
+        if sana.lower() in sanat:
+            print(f'{sana} ', end='')
+        else:
+            print(f'*{sana}* ', end='')
+    print()
+
+
+if __name__ == '__main__':
+    main()
