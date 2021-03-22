a3ea36f51b7b22c5b2b4b1f1970fcec145b5a8aa
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-22 11:05:35 +0200

    aineston suodatus

--- osa06-12_aineiston_suodatus/src/aineiston_suodatus.py
+++ osa06-12_aineiston_suodatus/src/aineiston_suodatus.py
@@ -1,1 +1,23 @@
 # tee ratkaisu tänne
+def suodata_laskut():
+    with open('laskut.csv') as tiedosto:
+        oikeat = []
+        vaarat = []
+        for rivi in tiedosto:
+            nimi, lasku, vastaus = rivi.split(';')
+            if eval(lasku) == int(vastaus):
+                oikeat.append(rivi)
+            else:
+                vaarat.append(rivi)
+
+        with open('oikeat.cvs', 'w') as tiedosto:
+            for rivi in oikeat:
+                tiedosto.write(rivi)
+
+        with open('vaarat.csv', 'w') as tiedosto:
+            for rivi in vaarat:
+                tiedosto.write(rivi)
+
+
+if __name__ == '__main__':
+    suodata_laskut()
