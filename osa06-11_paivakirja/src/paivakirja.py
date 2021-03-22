c1da687cdbe85ec20cf20007fa3d1a1c29a607df
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-22 11:05:11 +0200

    paivakirja

--- osa06-11_paivakirja/src/paivakirja.py
+++ osa06-11_paivakirja/src/paivakirja.py
@@ -1,1 +1,31 @@
 # tee ratkaisu tänne
+
+def kirjoita():
+    with open('paivakirja.txt', 'a') as tiedosto:
+        merkinta = input('Anna merkintä: ')
+        tiedosto.write(f'{merkinta}\n')
+        print('Päiväkirja tallennettu')
+
+
+def lue():
+    with open('paivakirja.txt') as tiedosto:
+        print('Merkinnät:')
+        for rivi in tiedosto:
+            print(rivi.strip())
+
+
+with open('paivakirja.txt', 'a') as tiedosto:
+    while True:
+        print('1 - lisää merkintä, 2 - lue merkinnät, 0 - lopeta')
+        valinta = input('Valinta: ')
+
+        if valinta == '0':
+            break
+
+        if valinta == '1':
+            kirjoita()
+
+        if valinta == '2':
+            lue()
+
+    print('Heippa!')
