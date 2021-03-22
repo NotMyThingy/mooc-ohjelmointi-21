106fb49a0b7f5e56f476ef32f993b748703fd84b
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-22 11:04:16 +0200

    omistuskirjoitus

--- osa06-10_omistuskirjoitus/src/omistuskirjoitus.py
+++ osa06-10_omistuskirjoitus/src/omistuskirjoitus.py
@@ -1,1 +1,7 @@
 # tee ratkaisu tänne
+nimi = input('Kenelle teos omistetaan: ')
+tiedosto = input('Mihin kirjoitetaan: ')
+
+with open(tiedosto, 'w') as t:
+    t.write(
+        f'Hei {nimi}, toivomme viihtyisiä hetkiä python-kurssimateriaalin parissa! Terveisin mooc.fi-tiimi')
