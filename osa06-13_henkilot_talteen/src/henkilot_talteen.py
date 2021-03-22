5b2a7263f262e98579fa6016f2e20d5a09937a1d
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-22 11:05:54 +0200

    henkilot talteen

--- osa06-13_henkilot_talteen/src/henkilot_talteen.py
+++ osa06-13_henkilot_talteen/src/henkilot_talteen.py
@@ -1,1 +1,8 @@
 # tee ratkaisu tänne
+def tallenna_henkilo(henkilo: tuple):
+    with open('henkilot.csv', 'a') as tiedosto:
+        tiedosto.write(f'{henkilo[0]};{henkilo[1]};{henkilo[2]}')
+
+
+if __name__ == '__main__':
+    tallenna_henkilo(("Kimmo Kimmonen", 37, 175.5))
