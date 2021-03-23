8b3334c63dbb999b776cc9ea9902bdd14c533ae5
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-23 14:07:22 +0200

    parametrien validointia

--- osa06-17_syotteen_luku/src/syotteen_luku.py
+++ osa06-17_syotteen_luku/src/syotteen_luku.py
@@ -1,1 +1,16 @@
 # tee ratkaisu tänne
+def lue(komento: str, ala: int, yla: int):
+    while True:
+        try:
+            luku = int(input('syötä luku: '))
+            if ala <= luku <= yla:
+                return luku
+        except:
+            pass
+
+        print(f'Syötteen on oltava kokonaisluku väliltä {ala}...{yla}')
+
+
+if __name__ == '__main__':
+    luku = lue("syötä luku: ", 5, 10)
+    print("syötit luvun:", luku)
--- osa06-18_parametrien_validointi/src/parametrien_validointi.py
+++ osa06-18_parametrien_validointi/src/parametrien_validointi.py
@@ -1,1 +1,13 @@
 # tee ratkaisu tänne
+def uusi_henkilo(nimi: str, ika: int):
+    if nimi == '' or nimi.count(' ') == 0 or len(nimi) > 40:
+        raise ValueError(f'nimi [{nimi}] ei kelpaa')
+    elif ika < 0 or ika > 150:
+        raise ValueError(f'ikä [{ika}] ei kelpaa')
+
+    return nimi, ika
+
+
+if __name__ == '__main__':
+    h = uusi_henkilo('massa masa', 154)
+    print(h)
