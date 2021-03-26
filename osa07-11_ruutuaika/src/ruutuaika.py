6554fb49133485908b4c46b18dd3b63d3859ceb0
NotMyThingy <miska.janhunen@protonmail.com>
2021-03-26 11:11:39 +0200

    valmis laskuri

--- osa07-11_ruutuaika/src/ruutuaika.py
+++ osa07-11_ruutuaika/src/ruutuaika.py
@@ -1,27 +1,47 @@
 # tee ratkaisu tänne,
 from datetime import datetime, timedelta
 
-try:
-    tiedosto = input('Tiedosto: ')
-except:
-    raise FileNotFoundError('Epäkelpo tiedoston nimi')
 
-try:
-    pvm = input('Aloituspäivä: ')
-    aloitus = datetime.strptime(pvm, '%d.%m.%Y')
-except:
-    raise ValueError('päivämäärä ei kelpaa')
-
-paivia = int(input('Montako päivää: '))
-lopetus = aloitus + timedelta(paivia)
-
-with open('kesakuun_loppu.txt', 'w') as tiedosto:
-    tiedosto.write(
-        f'Ajanjakso: {aloitus.strftime("%d.%m.%Y")} - {lopetus.strftime("%d.%m.%Y")}')
-
-    print('Anna ruutuajat kunakin päivänä minuutteina (TV tietokone mobiililaite):')
+def lue_ruutuaika(paivia: int, aloitus_pvm: datetime):
+    ruutuajat = {}
     for i in range(paivia):
-        aika = aloitus.strftime("%d.%m.%Y")
+        aika = aloitus_pvm.strftime("%d.%m.%Y")
         ajat = input(f'Ruutuaika {aika}: ')
         ruutuajat[aika] = ajat.replace(' ', '/')
-        aloitus += timedelta(1)
+        aloitus_pvm += timedelta(1)
+
+    return ruutuajat
+
+
+def laske_minuutit(ruutuaika: dict):
+    minuutit = []
+    for pv in ruutuaika:
+        summa = 0
+        ajat = ruutuaika[pv].split('/')
+        for aika in ajat:
+            summa += int(aika)
+        minuutit.append(summa)
+
+    return minuutit
+
+
+def main():
+    tiedosto = input('Tiedosto: ')
+    pvm = input('Aloituspäivä: ')
+    aloitus = datetime.strptime(pvm, '%d.%m.%Y')
+    paivia = int(input('Montako päivää: '))
+
+    print('Anna ruutuajat kunakin päivänä minuutteina (TV tietokone mobiililaite):')
+    ruutuaika = lue_ruutuaika(paivia, aloitus)
+    minuutit = laske_minuutit(ruutuaika)
+
+    with open(tiedosto, 'w') as tiedosto:
+        tiedosto.write(f'Ajanjakso: {aloitus.strftime("%d.%m.%Y")} - {(aloitus + timedelta(paivia -1)).strftime("%d.%m.%Y")}\n')
+        tiedosto.write(f'Yht. minuutteja: {sum(minuutit)}\n')
+        tiedosto.write(f'Keskim. minuutteja: {sum(minuutit) / len(minuutit):.1f}\n')
+        for paiva in ruutuaika:
+            tiedosto.write(f'{paiva}: {ruutuaika[paiva]}\n')
+
+
+if __name__ == '__main__':
+    main()
