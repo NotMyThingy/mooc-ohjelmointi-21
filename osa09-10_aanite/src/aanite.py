f41f74d74b343595d1e03e49a70f62cbd0920ee3
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-06 09:20:29 +0300

    luokka mallintaa yksittäistä äänitettä. Luokalla on yksi piilotettu attribuutti, kokonaislukutyyppinen __pituus.

---
+++ osa09-10_aanite/src/aanite.py
@@ -0,0 +1,26 @@
+# TEE RATKAISUSI TÄHÄN:
+class Aanite:
+
+    def __init__(self, pituus) -> None:
+        if pituus > 0:
+            self.__pituus = pituus
+        else:
+            raise ValueError('nolla tai negatiivinen luku ei kelpaa')
+
+    @property
+    def pituus(self):
+        return self.__pituus
+
+    @pituus.setter
+    def pituus(self, pituus):
+        if pituus > 0:
+            self.__pituus = pituus
+        else:
+            raise ValueError('nolla tai negatiivinen luku ei kelpaa')
+
+
+if __name__ == "__main__":
+    the_wall = Aanite(43)
+    print(the_wall.pituus)
+    the_wall.pituus = 44
+    print(the_wall.pituus)
