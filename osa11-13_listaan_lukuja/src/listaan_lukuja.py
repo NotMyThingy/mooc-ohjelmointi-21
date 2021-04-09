bc3f21532590978d5532930e5c27a79bcf819487
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-09 17:59:33 +0300

    rekursiivinen funktio lisää listaan lukuja niin kauan, kunnes listan pituus on viidellä jaollinen.

--- osa11-13_listaan_lukuja/src/listaan_lukuja.py
+++ osa11-13_listaan_lukuja/src/listaan_lukuja.py
@@ -1,1 +1,14 @@
 # TEE RATKAISUSI TÄHÄN:
+def listaan_lukuja(luvut: list):
+    if len(luvut) % 5 == 0:
+        return luvut
+
+    viimeinen = luvut[-1]
+    luvut.append(viimeinen + 1)
+    listaan_lukuja(luvut)
+
+
+if __name__ == "__main__":
+    luvut = [1, 3, 4, 5, 10, 11]
+    listaan_lukuja(luvut)
+    print(luvut)
