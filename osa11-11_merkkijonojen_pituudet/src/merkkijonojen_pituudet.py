ddd5da3e9872391a7c76b47f2b394c71ec3ade5d
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-09 10:58:25 +0300

    Funktio palauttaa sanakirjakoosteen avulla sanakirjan, jossa avaimina on listan merkkijonot ja arvoina merkkijonojen pituudet.

--- osa11-11_merkkijonojen_pituudet/src/merkkijonojen_pituudet.py
+++ osa11-11_merkkijonojen_pituudet/src/merkkijonojen_pituudet.py
@@ -1,1 +1,10 @@
 # TEE RATKAISUSI TÄHÄN:
+def pituudet(merkkijonot: list):
+    return {mjono: len(mjono) for mjono in merkkijonot}
+
+
+if __name__ == '__main__':
+    sanalista = ["suo", "kuokka", "python", "ja", "koodari"]
+
+    sanojen_pituudet = pituudet(sanalista)
+    print(sanojen_pituudet)
