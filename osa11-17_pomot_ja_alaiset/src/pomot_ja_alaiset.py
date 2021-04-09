5f70f9bacae12576aab6157dcfe01595f841ca40
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-09 18:08:44 +0300

    funktio laskee rekursiivisesti annetun työntekijän alaisten määrän

--- osa11-17_pomot_ja_alaiset/src/pomot_ja_alaiset.py
+++ osa11-17_pomot_ja_alaiset/src/pomot_ja_alaiset.py
@@ -6,3 +6,30 @@ class Tyontekija:
 
     def lisaa_alainen(self, tyontekija: 'Tyontekija'):
         self.alaiset.append(tyontekija)
+
+
+def laske_alaiset(tyontekija: Tyontekija):
+    alaiset = len(tyontekija.alaiset)
+    if alaiset == 0:
+        return alaiset
+
+    for alainen in tyontekija.alaiset:
+        alaiset += laske_alaiset(alainen)
+    return alaiset
+
+
+if __name__ == "__main__":
+    t1 = Tyontekija("Sasu")
+    t2 = Tyontekija("Erkki")
+    t3 = Tyontekija("Matti")
+    t4 = Tyontekija("Emilia")
+    t5 = Tyontekija("Antti")
+    t6 = Tyontekija("Kjell")
+    t1.lisaa_alainen(t4)
+    t1.lisaa_alainen(t6)
+    t4.lisaa_alainen(t2)
+    t4.lisaa_alainen(t3)
+    t4.lisaa_alainen(t5)
+    print(laske_alaiset(t1))
+    print(laske_alaiset(t4))
+    print(laske_alaiset(t5))
