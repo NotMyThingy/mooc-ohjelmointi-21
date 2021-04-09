20dfe95fc12503987627798cfb4d110021d47c5e
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-09 12:03:20 +0300

    Funktio palauttaa koosteen avulla sanakirjan, joka sisältää tiedostossa olevien sanojen esiintymislukumäärän niiden sanojen osalta, joilla on vähintään toisen parametrin raja verran esiintymiä.

--- osa11-12_yleisimmat_sanat/src/yleisimmat_sanat.py
+++ osa11-12_yleisimmat_sanat/src/yleisimmat_sanat.py
@@ -1,1 +1,17 @@
 # TEE RATKAISUSI TÄHÄN:
+from string import punctuation
+
+
+def yleisimmat_sanat(tiedoston_nimi: str, raja: int):
+    with open(tiedoston_nimi) as tiedosto:
+        sisalto = tiedosto.read()
+        sisalto = sisalto.replace('\n', '')
+        for merkki in punctuation:
+            sisalto = sisalto.replace(merkki, '')
+
+        sanat = sisalto.split()
+        return {sana: sanat.count(sana) for sana in sanat if sanat.count(sana) >= raja}
+
+
+if __name__ == "__main__":
+    print(yleisimmat_sanat("comprehensions.txt", 3))
