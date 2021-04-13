0427e2cae1962e7a44697f9a653f539394cc0b1f
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-13 12:09:33 +0300

    Funktio järjestää parametrinaan saamat tuotteet varastosaldojen mukaiseen kasvavaan järjestykseen

---
+++ osa12-01_varastosaldo/src/varastosaldo.py
@@ -0,0 +1,14 @@
+def jarjesta_varastosaldon_mukaan(alkiot: list):
+
+    # apufunktio
+    def varastosaldo(alkio: tuple):
+        return alkio[2]
+
+    return sorted(alkiot, key=varastosaldo)
+
+
+if __name__ == "__main__":
+    tuotteet = [("banaani", 5.95, 12), ("omena", 3.95, 3), ("appelsiini", 4.50, 2), ("vesimeloni", 4.95, 22)]
+
+    for tuote in jarjesta_varastosaldon_mukaan(tuotteet):
+        print(f"{tuote[0]} {tuote[2]} kpl")
