330d3b68025ecafdf70554f8e792bc30a6273986
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-19 13:41:42 +0300

    säännöllisten lausekkeiden kertausta

---
+++ osa12-14_saannolliset_lausekkeet/src/saannolliset_lausekkeet.py
@@ -0,0 +1,21 @@
+# TEE RATKAISUSI TÄHÄN:
+import re
+
+
+def on_viikonpaiva(merkkijono: str):
+    return re.search('ma|ti|ke|to|pe|la|su', merkkijono) is not None
+
+
+def kaikki_vokaaleja(merkkijono: str):
+    return re.search('^[aeiouyöäå]*$', merkkijono) is not None
+
+
+def kellonaika(merkkijono: str):
+    return re.search('([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]', merkkijono) is not None
+
+
+if __name__ == "__main__":
+    print(kellonaika("12:43:01"))
+    print(kellonaika("AB:01:CD"))
+    print(kellonaika("17:59:59"))
+    print(kellonaika("33:66:77"))
