8dec2d34742743bc318861730b5414503caf670c
NotMyThingy <miska.janhunen@protonmail.com>
2021-04-27 16:58:01 +0300

    no sound, adjust txt pos, esc instuctions missing, rock needs to be replaced

--- osa13-17_asteroidit/src/main.py
+++ osa13-17_asteroidit/src/main.py
@@ -1,67 +1,147 @@
 import pygame
 from random import randint
 
+# import pygame.locals for easier access to key coordinates
+from pygame.locals import (
+    RLEACCEL,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+POINTS = 0
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.image = pygame.image.load('../assets/robo.png').convert()
+        self.rect = self.image.get_rect(
+            center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - self.image.get_height() / 2 - 20)
+        )
+
+    # move the player based on user input
+    def update(self, keys_pressed) -> None:
+        if keys_pressed[K_LEFT] and self.rect.left > 0:
+            self.rect.move_ip(-8, 0)
+        if keys_pressed[K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+            self.rect.move_ip(8, 0)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.image = pygame.image.load('../assets/kivi.png').convert()
+        self.image.set_colorkey((0, 0, 0), RLEACCEL)
+        self.rect = self.image.get_rect(
+            center=(
+                randint(0, SCREEN_WIDTH),
+                -randint(20, 100),
+            )
+        )
+
+    # move the sprite
+    def update(self) -> None:
+        self.rect.move_ip(0, 1)
+
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Floor, self).__init__()
+        self.image = pygame.Surface((SCREEN_WIDTH, 20))
+        self.image.fill((125, 125, 125))
+        self.rect = self.image.get_rect(
+            center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10)
+        )
+
+
 pygame.init()
-width, height = 640, 480
-display = pygame.display.set_mode((width, height))
 
-robo = pygame.image.load("robo.png")
-rock = pygame.image.load('kivi.png')
+pygame.font.init()
+font = pygame.font.SysFont("FreeSans", 20)
 
-robo_x = int(width / 2 - robo.get_width() / 2)
-robo_y = height - robo.get_height()
+clock = pygame.time.Clock()
 
-right = False
-left = False
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-amount = 20
-rocks = []
-for i in range(amount):
-    rocks.append([-1000, height])
+# background image
+bg = pygame.image.load('../assets/bg.jpeg')
 
-ticker = pygame.time.Clock()
+# a custom event for adding a new rock. +1 ensures it's going to be unique event
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 2000)
 
-while True:
+# player instance
+robo = Player()
+# floor
+floor = Floor()
+
+# Groups for enemy sprites(rocks) and all sprites
+# - enemies for collision detection and pos updates
+# - all_sprites for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(robo)
+all_sprites.add(floor)
+
+running = True
+speed = 30
+
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                right = True
-            if event.key == pygame.K_LEFT:
-                left = True
+        elif event.type == ADDENEMY:
+            new_rock = Enemy()
+            enemies.add(new_rock)
+            all_sprites.add(new_rock)
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                right = False
-            if event.key == pygame.K_LEFT:
-                left = False
+    # get a set of pressed keys, check for user input and update player pos
+    pressed_keys = pygame.key.get_pressed()
+    robo.update(pressed_keys)
 
-    if right:
-        robo_x += 2
-    if left:
-        robo_x -= 2
+    # update rock positions
+    enemies.update()
 
-    robo_x = max(robo_x, 0)
-    robo_x = min(robo_x, width - robo.get_width())
+    # fill the screen
+    screen.fill((0, 0, 0))
 
-    for i in range(amount):
-        if rocks[i][1] + rock.get_height() < height:
-            rocks[i][1] += 1
-        else:
-            if rocks[i][0] < -rock.get_width() or rocks[i][0] > width:
-                rocks[i][0] = randint(0, width - rock.get_width())
-                rocks[i][1] = -randint(100, 1000)
-    # is it a hit
-    for i in range(amount):
-        if rocks[i][0] in range(robo_x, robo_x + robo.get_width()) and rocks[i][1] in range(robo_y, robo_y + robo.get_height()):
-            print('hit')
+    # draw background
+    screen.blit(bg, (0, 0))
 
-        display.fill((0, 0, 0))
-        for i in range(amount):
-            display.blit(rock, (rocks[i][0], rocks[i][1]))
-        display.blit(robo, (robo_x, robo_y))
-        pygame.display.flip()
+    # draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.image, entity.rect)
 
-    ticker.tick(60)
+    # check if the robot collides with a rock
+    for entity in enemies:
+        if pygame.sprite.collide_rect(robo, entity):
+            entity.kill()
+            POINTS += 1
+        print(POINTS)
+
+    # check if rock hits the ground
+    if pygame.sprite.spritecollideany(floor, enemies):
+        running = False
+
+    # update points
+    text = font.render(f'pisteet: {POINTS}', True, (255, 255, 255))
+    screen.blit(text, (520, SCREEN_HEIGHT - 20))
+
+    # update the screen
+    pygame.display.flip()
+
+    # set fps, aka speed
+    clock.tick(speed)
+
+# end the game
+pygame.quit()
